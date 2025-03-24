3fe6d89c539769017f79f610e13e494be7353598
HyeJinSeok <u3afguu@naver.com>
2025-03-24 10:35:38 +0900

    task1. project push

---
+++ step07_cicd/src/main/java/eud/fisa/ce/Step07CicdApplication.java
@@ -0,0 +1,13 @@
+package eud.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step07CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step07CicdApplication.class, args);
+	}
+
+}
